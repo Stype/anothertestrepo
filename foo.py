4ae0bfbf0ba305ea8e87045e04f88bbe83b27e1a
Chris Steipp <csteipp@lyft.com>
2017-02-06 12:36:20 -0800

    Really trigger bandit this time

--- foo.py
+++ foo.py
@@ -1,11 +1,19 @@
 import os
 import md5
+import hashlib
 
 # foo asdf
 
-def bar():
+SECRET_TOKEN='02B635D555BD82584C4672067347E8F28E763F64F43DAF9F24728484477F4E26'
+
+def bar(msg):
     """
     bar:
     """
-    return md5("bar")
+    secret = "mysecretstring"
+    hasher = hashlib.md5()
+    hasher.update(msg)
+    return hasher.hexdigest() == SECRET_TOKEN
+
+
 
